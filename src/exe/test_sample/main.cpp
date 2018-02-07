c96d32f10569e2653abf90d2734e16b7aeacba85
Johannes Braun <johannesbraun@uni-koblenz.de>
2018-02-08 00:04:57 +0100

    Test some configurations

--- src/exe/test_sample/main.cpp
+++ src/exe/test_sample/main.cpp
@@ -5,10 +5,11 @@
 #include <jpu/memory>
 #include <io/window.hpp>
 #include <jpu/data>
+#include <stack>
 
-constexpr int Logn = 18;
-std::vector<uint32_t> data1(1 << Logn);
-std::vector<uint32_t> data2(1 << Logn);
+constexpr int Logn = 13;
+std::vector<float> data1(1 << Logn);
+std::vector<float> data2(1 << Logn);
 
 void bitonic(int logn, uint32_t* a, size_t len)
 {
@@ -33,6 +34,118 @@ void bitonic(int logn, uint32_t* a, size_t len)
     }
 }
 
+class GPU_Clock
+{
+public:
+    /**
+    * \brief Retrieves/Creates the Singleton instance.
+    * \return the instance.
+    */
+    static GPU_Clock& instance();
+    ~GPU_Clock();
+
+    GPU_Clock(GPU_Clock&&) = delete;
+    GPU_Clock& operator=(GPU_Clock&& other) = delete;
+    // automatically no copy operations now
+
+    /**
+    * \brief Start the Clock.
+    */
+    void start();
+
+    /**
+    * \brief End the Clock.
+    * \return The time measured in Nanoseconds.
+    */
+    GLuint64 end();
+
+private:
+    /**
+    * \brief Wrapper-Class for GPU-Queries with GL_TIME_ELAPSED.
+    * Cannot be nested, thus this class is private because GPU_Clock works around that problem.
+    */
+    class TimerQuery
+    {
+    public:
+
+        TimerQuery() { glGenQueries(1, &m_id); };
+        explicit TimerQuery(GLuint id) : m_id(id) {};
+        ~TimerQuery() { glDeleteQueries(1, &m_id); };
+
+        TimerQuery(TimerQuery&&) = delete;
+        TimerQuery& operator=(TimerQuery&& other) = delete;
+        // automatically no copy operations now
+
+        /**
+        * \brief Begins the Query GL_TIME_ELAPSED.
+        */
+        void issue() const { glBeginQuery(GL_TIME_ELAPSED, m_id); };
+
+        /**
+        * \brief Ends the Query GL_TIME_ELAPSED, waits for the result and returns it.
+        * \return The elapsed time since issue(), measured in Nanoseconds.
+        */
+        GLuint64 get() const
+        {
+            glEndQuery(GL_TIME_ELAPSED);
+            GLuint64 result;
+            glGetQueryObjectui64v(m_id, GL_QUERY_RESULT, &result);
+            return result;
+        };
+
+    private:
+        GLuint m_id;
+    };
+
+    GPU_Clock();
+
+    TimerQuery m_query;
+    int m_current_index = 0;
+    std::vector<GLuint64> m_times;
+    std::stack<int> m_start_indices;
+};
+
+GPU_Clock::GPU_Clock()
+{
+}
+
+GPU_Clock& GPU_Clock::instance()
+{
+    static GPU_Clock _instance;
+    return _instance;
+}
+
+void GPU_Clock::start()
+{
+    if (!m_start_indices.empty())
+    {
+        m_times.push_back(m_query.get());
+        m_current_index++;
+    }
+    m_start_indices.push(m_current_index);
+    m_query.issue();
+}
+
+GLuint64 GPU_Clock::end()
+{
+    m_current_index++;
+    m_times.push_back(m_query.get());
+    int index = m_start_indices.top();
+    m_start_indices.pop();
+    GLuint64 elapsed_time = 0;
+    elapsed_time = std::accumulate(m_times.begin() + index, m_times.begin() + m_current_index, elapsed_time);
+    if (m_start_indices.empty())
+    {
+        m_current_index = 0;
+        m_times.clear();
+    }
+    else { m_query.issue(); }
+    return elapsed_time;
+}
+
+GPU_Clock::~GPU_Clock() {}
+
+
 jpu::ref_ptr<io::window> main_window;
 jpu::named_vector<std::string, jpu::ref_ptr<gl::graphics_pipeline>> graphics_pipelines;
 
@@ -40,8 +153,8 @@ int main(int argc, const char** argv)
 {
     gl::setup_shader_paths("../shaders");
 
-    glfwWindowHint(GLFW_SAMPLES, 8);
-    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+   // glfwWindowHint(GLFW_SAMPLES, 8);
+   // glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
     main_window = jpu::make_ref<io::window>(io::api::opengl, 1280, 720, "My Window");
     main_window->load_icon("../res/ui/logo.png");
     glfwSwapInterval(0);
@@ -68,11 +181,19 @@ int main(int argc, const char** argv)
         if (main_window->gui()->mouse_button_action(btn, action, mods))
             return;
     });
+    data2 = data1;
 
     std::generate(data1.begin(), data1.end(), []() { return rand(); });
-    data2 = data1;
     auto buf = jpu::make_ref<gl::buffer>(data1);
     buf->bind(0, GL_SHADER_STORAGE_BUFFER);
+
+
+
+
+
+
+    auto rdx_bckt = jpu::make_ref<gl::compute_pipeline>(
+        jpu::make_ref<gl::shader>(gl::shader_root_path / "sort/radix_bucket.comp"));
 
     auto cmp = jpu::make_ref<gl::compute_pipeline>(
         jpu::make_ref<gl::shader>(gl::shader_root_path / "sort/bitonic.comp"));
@@ -82,9 +203,11 @@ int main(int argc, const char** argv)
     auto puni = stage->get_uniform<int>("p");
     auto quni = stage->get_uniform<int>("q");
     auto duni = stage->get_uniform<int>("d");
+    auto count = stage->get_uniform<int>("count");
 
-    auto t = std::chrono::steady_clock::now();
     const int trg = data1.size() >> 1;
+    GPU_Clock::instance().start();
+    int i = 0;
     for (int p = 0; p < Logn; ++p)
     {
         puni = p;
@@ -93,15 +216,18 @@ int main(int argc, const char** argv)
             const int d = 1 << (p - q);
             duni = d;
             quni = q;
+            count = trg;
             cmp->dispatch(trg);
             glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+            ++i;
         }
     }
-    glFinish();
-    log_i << (std::chrono::steady_clock::now() - t).count();
+    log_i << GPU_Clock::instance().end();
 
-    t = std::chrono::steady_clock::now();
-    //bitonic(Logn, data2.data(), data2.size());
+    log_i << "I is " << i;
+
+    auto t = std::chrono::steady_clock::now();
+    // bitonic(Logn, reinterpret_cast<uint32_t*>(data2.data()), data2.size());
     std::sort(data2.begin(), data2.end());
     log_i << (std::chrono::steady_clock::now() - t).count();
 
