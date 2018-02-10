3d1997d7b3a4053a0a9b4bb1f3c914c6f180f834
Johannes Braun <johannesbraun@uni-koblenz.de>
2018-02-10 12:37:00 +0100

    Build an async response server.

--- src/exe/asio_client/main.cpp
+++ src/exe/asio_client/main.cpp
@@ -1,44 +1,50 @@
 #include <array>
 #include <iostream>
 #include <asio/asio.hpp>
+#include <jpu/log>
 
 using asio::ip::tcp;
 
-int main()
+int main() try
 {
-    try
+    asio::io_service service;
+    tcp::resolver resolver(service);
+
+    tcp::resolver::query query("localhost", "13");
+    tcp::resolver::iterator endpoints = resolver.resolve(query);
+    tlog_i("TCP Client") << "Resolve connection to localhost:13...";
+
+    for (;;)
     {
-        asio::io_service service;
-        tcp::resolver resolver(service);
+        tcp::socket socket(service);
+        asio::connect(socket, endpoints);
+        std::string message = "";
+        tlog_h("TCP Client") << "Type in a nice message (nice = begins with Hello): ";
+        std::getline(std::cin, message, '\n');
 
-        tcp::resolver::query query("localhost", "13");
-        tcp::resolver::iterator endpoints = resolver.resolve(query);
-        
+        asio::error_code ignored_error;
+        asio::write(socket, asio::buffer(message + "\n\r\n\r"), ignored_error);
 
-        for (;;)
-        {
-            tcp::socket socket(service);
-            asio::connect(socket, endpoints);
+        /*for (;;)
+        {*/
+            std::array<char, 128> buffer;
+            asio::error_code error;
+            size_t len = socket.read_some(asio::buffer(buffer), error);
 
-            for (;;)
-            {
-                std::array<char, 128> buffer;
-                asio::error_code error;
-                size_t len = socket.read_some(asio::buffer(buffer), error);
+           /* if (error == asio::error::eof)
+                break;
+            if (error)
+                throw asio::system_error(error);*/
 
-                if (error == asio::error::eof)
-                    break;
-                if (error)
-                    throw asio::system_error(error);
-
-                std::cout.write(buffer.data(), len);
-            }
-        }
-    } catch(std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
+            std::stringstream message_buffer;
+            message_buffer.write(buffer.data(), len);
+            tlog_i("TCP Client") << "Received Message: " << message_buffer.str() << '\n';
+        /*}*/
     }
-
+}
+catch (std::exception& e)
+{
+    std::cerr << e.what() << '\n';
     system("pause");
     return 0;
 }
--- src/exe/asio_server/main.cpp
+++ src/exe/asio_server/main.cpp
@@ -1,17 +1,31 @@
+#include <array>
+#include <iostream>
+#include <sstream>
 #include <ctime>
 #include <iostream>
 #include <string>
 #include <asio/asio.hpp>
+#include <jpu/log>
+#include "tcp_server.hpp"
 
 using asio::ip::tcp;
 
-std::string make_daytime_string()
+int main() try
 {
-    std::time_t now = std::time(nullptr);
-    return std::ctime(&now);
+    asio::io_service service;
+    net::tcp_server server(service);
+    service.run();
+} 
+catch (std::exception& e)
+{
+    tlog_e("Server") << e.what();
+    system("pause");
 }
 
-int main() try
+
+
+
+int main_02() try
 {
     asio::io_service service;
     tcp::acceptor acceptor(service, tcp::endpoint(tcp::v4(), 13));
@@ -21,7 +35,20 @@ int main() try
         tcp::socket socket(service);
         acceptor.accept(socket);
 
-        std::string message = make_daytime_string();
+        std::array<char, 128> buffer;
+        asio::error_code error;
+        size_t len = socket.read_some(asio::buffer(buffer), error);
+
+        if (error == asio::error::eof)
+            break;
+        if (error)
+            throw asio::system_error(error);
+
+        std::stringstream message_buffer;
+        message_buffer.write(buffer.data(), len);
+        tlog_i("Server") << "Received Message: " << message_buffer.str();
+
+        std::string message = "Yep I am here.";
         asio::error_code ignored_error;
         asio::write(socket, asio::buffer(message), ignored_error);
     }
@@ -30,5 +57,6 @@ int main() try
 catch(std::exception& e)
 {
     std::cerr << e.what() << '\n';
+    system("pause");
     return 1;
 }
