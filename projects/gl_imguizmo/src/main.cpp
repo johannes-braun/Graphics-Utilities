19b0096b7cbf9ab9e1f97a9e2545b0ebd4a298bb
Johannes Braun <johannesbraun@uni-koblenz.de>
2018-06-10 23:47:01 +0200

    Add ECS

--- projects/gl_imguizmo/src/main.cpp
+++ projects/gl_imguizmo/src/main.cpp
@@ -1,43 +1,18 @@
-#include <gfx/camera.hpp>
 #include <gfx/imgui.hpp>
 #include <gfx/window.hpp>
-
-#include <imgui/ImGuizmo.h>
+#include <gfx/ecs/system.hpp>
 
 int main()
 {
     auto       win = std::make_shared<gfx::window>(gfx::apis::opengl::name, "ImGuizmo", 1280, 720);
     gfx::imgui gui(win);
 
-    gfx::camera            camera;
-    gfx::camera_controller controller(win);
-    glm::vec3              pos1(0, 0, 0);
-    glm::vec3              pos2(1, 0, 0);
-    glm::vec3              pos3(1, 1, 0);
-    glm::vec3              pos4(0, 1, 0);
-    glm::vec2              size(1280, 720);
 
-    gfx::transform transform;
+    
 
     while(win->update())
     {
         gui.new_frame();
-        ImGuizmo::BeginFrame();
-        ImGuizmo::SetRect(0, 0, 1280, 720);
-        glm::mat4 tfmat = transform;
-        ImGuizmo::Manipulate(glm::value_ptr(camera.view()),
-                             glm::value_ptr(camera.projection()),
-                             ImGuizmo::ROTATE,
-                             ImGuizmo::WORLD,
-                             glm::value_ptr(tfmat));
-        transform = tfmat;
-
-        controller.update(camera);
-
-
-        ImGui::Begin("touiashd");
-        ImGui::End();
-
 
         gui.render();
     }
---
+++ utilities/src/gfx/ecs/ecs.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "system.hpp"
+#include <unordered_map>
+
+namespace gfx::ecs
+{
+    using entity = std::vector<std::pair<id_t, uint32_t>>;
+    using indexed_entity = std::pair<uint32_t, entity>;
+class ecs
+{
+public:
+    ecs() = default;
+    ~ecs() {}
+
+    entity_handle create_entity(component_base* components, const uint32_t* component_ids, size_t count);
+    void delete_entity(entity_handle handle);
+    
+    template<typename Component>
+    void add_component(entity_handle handle, Component* component);
+    
+    template<typename Component>
+    void remove_component(entity_handle handle);
+
+    template<typename Component>
+    void get_component(entity_handle handle);
+
+    void add_system(system_base* system)
+    {
+        _systems.push_back(system);
+    }
+    void remove_system(system_base* system);
+    void update(double delta);
+
+private:
+    std::vector<system_base*> _systems;
+    std::unordered_map<id_t, std::vector<component_base*>> _components;
+    std::vector<indexed_entity*> _entities;
+
+    static indexed_entity* as_entity_ptr(entity_handle handle)
+    {
+        return static_cast<indexed_entity*>(handle);
+    }
+
+    static uint32_t index_of(entity_handle handle)
+    {
+        return as_entity_ptr(handle)->first;
+    }
+
+    static entity& as_entity(entity_handle handle)
+    {
+        return as_entity_ptr(handle)->second;
+    }
+};
+} // namespace gfx::ecs
