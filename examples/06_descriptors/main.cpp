f10da4540094fba7db865c4b359200a8156f1907
Johannes Braun <johannesbraun@uniko.uni-koblenz.de>
2018-10-11 15:13:58 +0200

    Try again with vulkan.hpp

--- examples/06_descriptors/main.cpp
+++ examples/06_descriptors/main.cpp
@@ -1,50 +1,129 @@
-#include <gfx/gfx.hpp>
+#define GLFW_EXPOSE_NATIVE_WIN32
+
+#include <cinttypes>
+#include <vector>
+#if _WIN32
+#define VK_USE_PLATFORM_WIN32_KHR
+#endif
+
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+
+#include <vulkan/vulkan.hpp>
+
+inline PFN_vkCreateDebugUtilsMessengerEXT impl_vkCreateDebugUtilsMessengerEXT;
+VkResult vkCreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+                                        const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pMessenger)
+{
+    return impl_vkCreateDebugUtilsMessengerEXT(instance, pCreateInfo, pAllocator, pMessenger);
+}
+inline PFN_vkDestroyDebugUtilsMessengerEXT impl_vkDestroyDebugUtilsMessengerEXT;
+void vkDestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT pMessenger, const VkAllocationCallbacks* pAllocator)
+{
+    impl_vkDestroyDebugUtilsMessengerEXT(instance, pMessenger, pAllocator);
+}
 
 namespace gfx {
 inline namespace v1 {
-enum class binding
+using u32 = uint32_t;
+using u16 = uint16_t;
+
+struct version_t
 {
-    invalid = 0,
-    buf_uniform,
-    buf_storage,
-    img_sampled,
-    img_storage
+    constexpr version_t(u16 major, u16 minor, u16 patch) : major(major), minor(minor), patch(patch) {}
+    constexpr version_t() : version_t(0, 0, 0) {}
+    constexpr operator u32&() { return *reinterpret_cast<u32*>(this); }
+    constexpr operator const u32&() const { return *reinterpret_cast<const u32*>(this); }
+
+    u16 major : 10;
+    u16 minor : 10;
+    u16 patch : 12;
 };
 
-struct bindings
+enum class api
 {
-    std::map<u32, binding> bindings;
+    vulkan
 };
 
-// This approach is kinda not good.
+namespace detail {
+constexpr version_t        engine_version = version_t(1, 0, 0);
+constexpr std::string_view engine_name    = "Graphics Utilities";
+}    // namespace detail
 
-struct set
+class instance
 {
-	void bind(...);
+public:
+    instance(std::string_view app_name, version_t app_version, bool debug, bool surface_support);
+    ~instance();
+
+	vk::Instance inst() const noexcept { return _instance.get(); }
+
+private:
+    vk::UniqueInstance               _instance;
+    vk::UniqueDebugUtilsMessengerEXT _debug_messenger;
 };
 
-struct desc
+instance::instance(std::string_view app_name, version_t app_version, bool debug, bool surface_support)
 {
-	std::unique_ptr<set> create_set(u32 index) 
-	{
+    vk::ApplicationInfo app_info;
+    app_info.apiVersion         = version_t(1, 1, 0);
+    app_info.applicationVersion = app_version;
+    app_info.engineVersion      = detail::engine_version;
+    app_info.pEngineName        = detail::engine_name.data();
+    app_info.pApplicationName   = app_name.data();
 
-	}
+    std::vector<const char*> extensions;
+    if (surface_support)
+    {
+        extensions.push_back("VK_KHR_surface");
+        extensions.push_back("VK_KHR_win32_surface");
+    }
+    if (debug) extensions.push_back("VK_EXT_debug_utils");
 
-    std::map<u32, bindings> sets;
-};
+    std::vector<const char*> layers;
+    if (debug) layers.push_back("VK_LAYER_LUNARG_standard_validation");
 
-void blub(u32 set, std::reference_wrapper<const gfx::desc> desc) {}
+    vk::InstanceCreateInfo instance_info({}, &app_info, static_cast<u32>(layers.size()), layers.data(), static_cast<u32>(extensions.size()),
+                                         extensions.data());
+    _instance = vk::createInstanceUnique(instance_info);
+
+    impl_vkCreateDebugUtilsMessengerEXT =
+        reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>(_instance->getProcAddr("vkCreateDebugUtilsMessengerEXT"));
+    impl_vkDestroyDebugUtilsMessengerEXT =
+        reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>(_instance->getProcAddr("vkDestroyDebugUtilsMessengerEXT"));
+
+    if (debug)
+    {
+        using sev = vk::DebugUtilsMessageSeverityFlagBitsEXT;
+        vk::DebugUtilsMessengerCreateInfoEXT debug_messenger_info(
+            {}, sev::eError | sev::eWarning | sev::eInfo, vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral,
+            [](VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
+               const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) -> VkBool32 {
+                switch (sev(messageSeverity))
+                {
+                case sev::eError: break;
+                case sev::eWarning: break;
+                case sev::eInfo: break;
+                }
+                return false;
+            });
+        _debug_messenger = _instance->createDebugUtilsMessengerEXTUnique(debug_messenger_info);
+    }
+}
+
+instance::~instance() {}
 }    // namespace v1
 }    // namespace gfx
 
 int main()
 {
-    gfx::sampler s;
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	auto* w = (glfwInit(), glfwCreateWindow(1280, 720, "aois", nullptr, nullptr));
+	HWND win = glfwGetWin32Window(w);
 
-    gfx::desc d;
-    d.sets[0] = {{{0, gfx::binding::buf_storage}, {1, gfx::binding::buf_uniform}}};
-    d.sets[1] = {{{0, gfx::binding::img_storage}}};
+	gfx::instance my_app("Application", gfx::version_t(1, 0, 0), true, true);
 
-	auto x = d.create_set(0);
-	x->bind(0, s);
+	vk::Win32SurfaceCreateInfoKHR ci;
+	ci.hwnd = win;
+	auto us = my_app.inst().createWin32SurfaceKHRUnique(ci);
 }
